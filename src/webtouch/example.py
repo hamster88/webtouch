2c968c89d16d5dcf04705b1b1f0e4e004c21aa0f
hamster88 <hamster88@users.noreply.github.com>
2025-02-01 21:09:28 +0800

    update example

--- src/webtouch/example.py
+++ src/webtouch/example.py
@@ -1,10 +1,14 @@
+# 编写脚本来调用 webtouch
+# 这里是在一个任务单元中执行多个相互依赖的http请求的例子
+
 from random import randint
-from webmiss import FetchTask, wm
+from webtouch import app, Task, FetchTask
 import requests
 
-wm.concurrent  = 16
+# 设置app参数，这些可以被命令行参数覆盖
+app.option.concurrent  = 16  # 并发数
 
-# 定义两个 URL 模板，用于模拟 API 请求
+
 # 第一个 URL 用于获取令牌（token），需要提供用户名和密码
 url1 = 'http://httpbin.org/uuid?action=get_token&username={}&password={}'
 # 第二个 URL 用于模拟操作（例如点赞），需要提供帖子 ID 和令牌
@@ -12,23 +16,25 @@ url2 = 'http://httpbin.org/anything/post/{}?action=like&token={}'
 
 # 定义一个自定义任务类 `CustomTask`，继承自 `FetchTask`
 class CustomTask(FetchTask):
-    # 定义属性，用于存储用户名、密码、令牌和帖子 ID
-    username: str  # 用户名
-    password: str  # 密码
-    token: str     # 获取的令牌
-    post_id: str   # 帖子 ID（任务 ID）
-
-    # 初始化方法，用于创建任务对象并初始化相关属性
     def __init__(self):
         # 调用父类的初始化方法，会自动计数并分配id
         super().__init__()
-        # 生成一些参数，格式化 URL1
+        
+        # 生成一些参数
         self.username = f'bot{randint(100, 995577)}'
         self.password = 'seceret'
-        self.url = url1.format(self.username, self.password)
+        self.password = 'seceret'
+        self.token = '' # 后面的参数要从前一个请求中获取
+        self.post_id = str(self.tid)
+        
+        # 设置父类的属性, 用于底层交互
+        self.url = url1.format(self.username, self.password) # 先设置第一个 url
+        self.title = f'Get token {self.username}'
+        self.detail = f'get {self.url}'
+        
 
-    # 定义任务的运行方法
-    def run(self):
+    # 任务的入口方法
+    def main(self):
         # 发送 HTTP GET 请求到 URL1，用于获取令牌
         res = requests.get(self.url)
         
@@ -43,6 +49,10 @@ class CustomTask(FetchTask):
         res = requests.get(self.url)  
         # 验证操作成功
         res.raise_for_status()
+        
+        # 用 self.error 报告一些非预期情况
+        # self.error = '429 Too Many Requests.'  
+        # self.error = 'Invalid response data.'  
 
-#  触发任务的执行
-# CustomTask.trigger()
+# 运行 app 
+app.main(CustomTask)
